ff3d594a97682908b95b1511dc573ab13031ab1d
[guille9492] <[guillermogonzalezmiranda@gmail.com]>
2024-02-22 09:33:09 +0100

    ejemploarray

---
+++ array copy.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $deportes = array (
+            "futbol","tenis","baloncesto","voleybol","natacion","maraton",
+            "lanzamiento de jabalina","ajedrez"
+        );
+
+      //foreach ($deportes as $deportes) {
+        //  echo $deportes . "<br>";
+       // }
+        
+        var_dump($deportes);
+        echo "<br>";
+        echo $deportes [0];
+       
+    ?>
+   
+</body>
+</html>
