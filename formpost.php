5f39ff5b1051968cc800bdcefaf3e389e8740af7
[guille9492] <[guillermogonzalezmiranda@gmail.com]>
2024-03-05 12:36:49 +0100

    new file:   formpost.php 	new file:   formulariopost.php

---
+++ formpost.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <form action="formulariopost.php" method="post">
+        Alumno: <input name="alumno" type="text">
+        Clase: <input name="clase" type="text">
+        <input type="submit" value="Enviar" />
+        <input type="reset" value="borrar"/>
+    </form>
+</body>
+</html>
---
+++ formulariopost.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+   
+    <?php
+        $alumno= $_POST['alumno'];
+        $clase = $_POST['clase'];
+        echo "El alumno es: " . $alumno;
+        echo "<br>";
+        echo "La clase es: " . $clase;
+    ?>
+</body>
+</html>
